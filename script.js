28910003883777f78d4d05312cc872eaed667d0c
Damian Gornik <34626736+damiangornik8@users.noreply.github.com>
2021-07-22 13:05:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 // Define all Variables
 
-// Generate button
+// Generate number (button)
 const generateBtn = document.getElementById('generateNumBtn');
-// Generated number display 
+// Display the generated number (button)
 const generatedNumDisplay = document.getElementById('generatedNumDisplay');
-// Generated number display title
+// Display the generated number (title)
 const generatedNumTitle = document.getElementById('generatedNumTitle');
 
-// Define a variable with a last generated number 
+// Define a variable to store the last generated number 
 let lastNumber;
 
-// Add event listener on 'Generate' button
+// Add an event listener on the 'Generate' button
 generateBtn.addEventListener('click', () => {
-  // Get the lower number of the range 
+  // Get the minimum number of the range 
   let minNum = parseInt(document.getElementById('minNum').value);
-  // Get the upper number of the range 
+  // Get the maximum number of the range 
   let maxNum = parseInt(document.getElementById('maxNum').value);
 
   // Create a random number within that range
   let randomNumber = Math.floor(Math.random() * ((maxNum-minNum)+1) + minNum);
 
-  // Make generated number display title visible
+  // Make the title visible
   generatedNumTitle.innerHTML = `Your number is:`;
 
-  // Display a generated number on the screen 
+  // Display the generated number to the user
   generatedNumDisplay.innerHTML = `${randomNumber}`;
 
   // If same number was created twice in a row, then try to generate a random number again
@@ -31,6 +31,6 @@ generateBtn.addEventListener('click', () => {
     let randomNumber = Math.floor(Math.random() * ((maxNum-minNum)+1) + minNum);
   }
 
-  // Make generated number a last created number   
+  // Make generated number to be the last created number   
   lastNumber = randomNumber;
 }, false);
